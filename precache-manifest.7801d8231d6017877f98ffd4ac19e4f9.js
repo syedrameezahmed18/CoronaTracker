db02f801df90741ca5685474cc0be4e7f28dc564
syedrameezahmed18 <noir.ramsam@gmail.com>
2020-06-20 18:42:36 +0500

    Updates

---
+++ precache-manifest.7801d8231d6017877f98ffd4ac19e4f9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd23b812490297d42c48e95e986e82c7",
+    "url": "/CoronaTracker/index.html"
+  },
+  {
+    "revision": "9d046f5932300fbd87ef",
+    "url": "/CoronaTracker/static/css/main.8ea5e182.chunk.css"
+  },
+  {
+    "revision": "1a548fcef338f7be762b",
+    "url": "/CoronaTracker/static/js/2.a833315a.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/CoronaTracker/static/js/2.a833315a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d046f5932300fbd87ef",
+    "url": "/CoronaTracker/static/js/main.e7d6b4e3.chunk.js"
+  },
+  {
+    "revision": "d2ffa35bb16ecf0e01bf",
+    "url": "/CoronaTracker/static/js/runtime-main.d5b6bec5.js"
+  },
+  {
+    "revision": "b05ebec720604a1efddd56ae4b03a772",
+    "url": "/CoronaTracker/static/media/a11.b05ebec7.jpg"
+  },
+  {
+    "revision": "63caf4f8cfa708d49aa41de0243440d5",
+    "url": "/CoronaTracker/static/media/a22.63caf4f8.jpg"
+  },
+  {
+    "revision": "fc047aab4f5fd34bed8a4644d6075637",
+    "url": "/CoronaTracker/static/media/a33.fc047aab.png"
+  },
+  {
+    "revision": "eb4b00b795441082f19227a2e879b38c",
+    "url": "/CoronaTracker/static/media/alert.eb4b00b7.jpg"
+  },
+  {
+    "revision": "111bc39813f523f91290876bfd36dae9",
+    "url": "/CoronaTracker/static/media/blood.111bc398.jpg"
+  },
+  {
+    "revision": "20839590417bd8c21c51aff7818e9e57",
+    "url": "/CoronaTracker/static/media/caution.20839590.jpg"
+  },
+  {
+    "revision": "08c05ef59a6f4c0d465b56020f52c6dc",
+    "url": "/CoronaTracker/static/media/corona.08c05ef5.jpg"
+  },
+  {
+    "revision": "48faa98189979a093b8a7d5540a5f435",
+    "url": "/CoronaTracker/static/media/covalt.48faa981.jpg"
+  },
+  {
+    "revision": "5a73cddc698caa41249436e8f329d561",
+    "url": "/CoronaTracker/static/media/cv.5a73cddc.jpg"
+  },
+  {
+    "revision": "82879f960b631b25b4f7f59714abe742",
+    "url": "/CoronaTracker/static/media/help.82879f96.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CoronaTracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
